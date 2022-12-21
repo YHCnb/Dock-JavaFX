d75dc9243d0bc3e2bc988a38bbe00dc23612bd1f
陈钰豪 <1262736427@qq.com>
2022-12-21 10:34:06 +0800

    Icon

--- Dock/src/main/java/com/yingtai/dock/IconImage.java
+++ Dock/src/main/java/com/yingtai/dock/IconImage.java
@@ -2,8 +2,36 @@ package com.yingtai.dock;
 
 import javafx.scene.image.Image;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+
 public class IconImage {
-    public static Image getIconImage(String realPath){
-        return new Image(realPath);
+    public static Image getIconImage(String realPath) throws IOException {
+        String imgPath="Dock/src/main/resources/com/yingtai/dock/img/";
+        new ProcessBuilder("Dock/src/main/resources/com/yingtai/dock/tool/IconExtractor.exe",realPath,imgPath).start();
+
+        int end = realPath.length() - 1;
+        while (realPath.charAt(end) != '.') end--;
+        int start = end;
+        while(realPath.charAt(start)!='\\'&&realPath.charAt(start)!='/') start--;
+        String name = realPath.substring(start + 1, end);
+
+        String imgP=imgPath+name+".png";
+//        Image image = new Image(new FileInputStream(imgP));
+        Image image = new Image(new FileInputStream(imgP));
+        return image;
+    }
+
+    public static void main(String[] args) {
+        String realPath="D:\\steam\\steam.exe";
+        try {
+            Image image = getIconImage(realPath);
+//            File f = new File("Dock/src/main/resources/com/yingtai/dock/img/test.png");
+//            ImageIO.write(image, "png", f);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
